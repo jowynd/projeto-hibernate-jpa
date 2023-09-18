2aa0124013e82348ccd00b072f46776403a2e359
jowynd <jowynd12@gmail.com>
2023-09-18 15:34:35 -0300

    Atualizar e remover

--- src/main/java/CategoriaDAO.java
+++ src/main/java/CategoriaDAO.java
@@ -13,4 +13,11 @@ public class CategoriaDAO {
     public void cadastrar(Categoria categoria) {
         this.em.persist(categoria);
     }
+    public void atualizar(Categoria categoria) {
+        this.em.merge(categoria);
+    }
+    public void remover(Categoria categoria) {
+        em.merge(categoria);
+        this.em.remove(categoria);
+    }
 }
--- src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -10,6 +10,9 @@ public class Categoria {
     private Long id;
     private String nome;
 
+    public Categoria() {
+    }
+
     public Long getId() {
         return id;
     }
